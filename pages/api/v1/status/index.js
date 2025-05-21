30699dd84759bff6c505fb34630a9a544182204a
Asafe Cabral Alves <98559029+asafebxd@users.noreply.github.com>
2025-05-21 22:03:00 +0000

    feat: finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,33 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result);
-  res
-    .status(200)
-    .json({ chave: "alunos do curso.dev sao pessoas acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query(`SHOW server_version;`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query(`SHOW max_connections;`);
+  const databaseMaxConnectionsResult =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsResult),
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,13 @@
 test("GET to /apip/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
-  console.log(res);
+
+  const resBody = await res.json();
+
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(resBody.dependencies.database.version).toEqual("16.8");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.open_connections).toEqual(1);
 });
