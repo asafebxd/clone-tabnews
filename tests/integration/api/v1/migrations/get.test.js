67b2c47a12b32457b5480c8e25a6485d7e86eab1
Asafe Cabral Alves <98559029+asafebxd@users.noreply.github.com>
2025-06-04 20:33:23 +0000

    make > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k -s command-jest 'next dev'  'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
    🛑 Waiting for Postgres Server Response
    Postgress accepting connections
    
    [jest] PASS tests/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.897 s, estimated 3 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. more robust with  and

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; CREATE schema public;");
-}
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/migrations");
