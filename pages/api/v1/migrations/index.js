eedab8c356fbbba974e992f9c9ec094c04e09922
asafebxd <alves.asafecabral@gmail.com>
2026-03-20 15:08:20 -0300

    chore: remove authentication from `migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,7 +7,8 @@ const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
 router.get(controller.canRequest("read:migration"), getHandler);
-router.post(controller.canRequest("create:migration"), postHandler);
+router.post(postHandler);
+// router.post(controller.canRequest("create:migration"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
