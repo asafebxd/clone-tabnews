26d088b4de2aba3d119a7257edca077d685dd4a3
asafebxd <alves.asafecabral@gmail.com>
2026-03-31 17:20:12 -0300

    refactor: inline `createRouter()` chain in all API route exports

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -3,12 +3,11 @@ import controller from "infra/controller";
 import session from "models/session";
 import user from "models/user";
 import authorization from "models/authorization";
-const router = createRouter();
 
-router.use(controller.injectAnonymousOrUser);
-router.get(controller.canRequest("read:session"), getHandler);
-
-export default router.handler(controller.errorHandlers);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .get(controller.canRequest("read:session"), getHandler)
+  .handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
   const userTryingToGet = req.context.user;
